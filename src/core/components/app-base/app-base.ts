17bc80bd4034f3da6f6cd358871ab64047b630ea
shinyks <syk2932@naver.com>
2024-10-29 17:30:40 +0900

    chore: Update app-base

--- src/core/components/app-base/app-base.ts
+++ src/core/components/app-base/app-base.ts
@@ -6,6 +6,7 @@ import { Toast } from '../toast/toast';
 
 export interface AppBaseProps extends ComponentProps {
   scaleInfo?: ScaleInfo;
+  allowUserHashControl?: boolean;
 }
 
 export class AppBase<Props extends AppBaseProps = AppBaseProps> extends Component<Props> {
@@ -16,17 +17,33 @@ export class AppBase<Props extends AppBaseProps = AppBaseProps> extends Componen
     return this.props.scaleInfo ?? {};
   }
 
+  get allowUserHashControl(): boolean {
+    return this.props.allowUserHashControl ?? true;
+  }
+
+  get zoomRate(): number {
+    return this.pageWrapper.pageScale.zoomRate;
+  }
+
+  get urlQueryObject(): any {
+    return this.pageWrapper.urlQueryObject;
+  }
+
   constructor(props: Props) {
-    super({...props,  ...style(styles, props), ...name('AppBase', props) });
+    super({...props, ...style(styles, props), ...name('AppBase', props) });
 
     window.addEventListener('resize', this.onResize.bind(this));
 
-    this.pageWrapper = this.addChild(PageWrapper, { scaleInfo: this.scaleInfo });
+    this.pageWrapper = this.addChild(PageWrapper, { scaleInfo: this.scaleInfo, allowUserHashControl: this.allowUserHashControl });
     this.toast = this.addChild(Toast);
   }
 
   onResize(): void {
     this.pageWrapper.updateLayout();
+  }
+
+  updateScaleInfo(scaleInfo: ScaleInfo): void {
+    this.pageWrapper.updateScaleInfo(scaleInfo);
   }
 
   setRoutes(routes: RouteInfo[]): void {
