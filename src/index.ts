b1bffbac08df911c143f9efeecc5a2b18b1e9ea0
shinyks <syk2932@naver.com>
2024-10-31 16:01:46 +0900

    chore: Clean css up

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
-import styles from './core/styles/default.scss';
-
-const styleElement = document.createElement('style');
-styleElement.innerHTML = styles;
-document.head.appendChild(styleElement);
-
 export * from './core/element';
 export * from './core/component';
 export * from './core/components';
